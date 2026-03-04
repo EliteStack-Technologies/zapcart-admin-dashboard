ced83c57328f970e0b119d9c123e213aab3011b1
Binshad-P <mohammedbinshad6268@gmail.com>
2026-03-04 19:53:33 +0530

    setup apk appliction

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zapcart.admin',
+  appName: 'ZapCart Admin',
+  webDir: 'dist'
+};
+
+export default config;
